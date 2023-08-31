bf98021df6f4c168bf36490c8ec801f6ac80e85a
HanifSauqi <muhammadhanifsauqi2004@mail.ugm.ac.id>
2023-08-31 10:01:15 +0700

    Latihan pertemuan 3

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,32 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        //comment 1
-    }
+private lateinit var binding: ActivityMainBinding
+//private var number = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+//
+//    with(binding) {
+//        textNumber.text = number.toString()
+//        btnCount.setOnClickListener{
+//            number++
+//            textNumber.text = number.toString()
+//        }
+//        btnToast.setOnClickListener{
+////            number--
+////            textNumber.text = number.toString()
+//            Toast.makeText(this@MainActivity,"count $number",
+//                Toast.LENGTH_SHORT).show()
+//        }
+//    }
+
+}
 }
